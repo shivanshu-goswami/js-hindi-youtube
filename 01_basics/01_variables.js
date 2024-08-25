2139b00035a0ab456f0af228bde08a4a9eb22353
shivanshu-goswami <goswamishivanshu47@gmail.com>
2024-08-25 20:07:55 +0000

    1st lecture Done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId=1223344 //semi-colon optional , const value cant be re-assigned
+let accountEmail="abc@gmail.com"
+accountPassword="9832"
+accountCity="Jaipur"
+let accountState;
+
+accountEmail="xyz@gmail.com"
+accountPassword="3442"
+accountCity="Patna"
+
+console.table([accountEmail,accountPassword,accountCity,accountState]);
