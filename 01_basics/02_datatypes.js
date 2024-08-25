7a3af667ef733000b64c7b69dfe264c04959ef89
shivanshu-goswami <goswamishivanshu47@gmail.com>
2024-08-25 20:31:18 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all new code as newer version of javascirpt but its reverse isn't possible
+ /* alert(3+3)  writing this in console window will pop up  the alert message but not here in NodeJs ,
+ to use alert here there is a different method */
+
+ // use mdn and tc39 documents for more knowledge
+
+ let name="shivanshu"
+ let age=20
+ let isLoggedIn= false
+
+ //Datatypes -> 
+
+ /*
+ A.primitive datatype
+
+ 1.number -> 2 to the power 53
+ 2.bigint
+ 3.string=>""
+ 4.boolean=>true/false
+ 5.null=>standalone value/ek type hai  
+ 6.undefined=> variable declared but value not assigned whereas null means empty value, we assign some variables value as null, eg let a=null
+ 7.symbol=> to find uniqueness, mostly used in react
+
+ B. Object
+ */
+
+ console.log(typeof undefined)
